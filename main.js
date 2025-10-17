fe887d0c7d3c87bf8c25666a6a4114a2a9240768
conradocamilo0 <conradocamilo018@gmail.com>
2025-10-17 08:21:52 -0500

    modulo os

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
-console.log("hola mundo");
+// console.log("hola mundo");
+
+// Crear un programa que identifique cuantos multiplos de 3 hay en un rango del 0 al 100 y que los imprima en consola
+
+// for (let i=0; i<= 100; i++) {
+//     if (i%3 ==0) {
+//         console.log(i);
+//     }
+// }
+
+import os from "os"
+
+function modulo_os() {
+    // console.log(os);
+
+    // const memory = (os.freemem / 1024) 
+    // console.log(memory / 1024);
+
+    // arch muestra la arquitectura del sistema
+    // console.log(os.arch());
+
+    // cpus muestra el tipo de procesador del equipo
+    // console.log(os.cpus());
+
+    // Muestra la prioridad del sistema operativo con el que arranca el equipo
+    // console.log(os.getPriority());
+
+    // muestra el nombre con el que esta asignado el equipo
+    // console.log(os.hostname());
+
+    // Muestra el promedio de procesos ejecutables que estan en segundo plano en alta carga
+    // console.log(os.loadavg());
+
+    // Muestra la plataforma universal de windows
+    // console.log(os.platform());
+
+    // muestra la version especifa del sistema operativo
+    // console.log(os.release());
+
+    // Muestra la prioridad del sistema o procesos 
+    // console.log(os.setPriority());
+
+    // Muestra la ruta de la carpeta de los archivos temporales
+    // console.log(os.tmpdir());
+
+    // Muestra la capacidad de RAM del equipo
+    // console.log(os.totalmem());
+
+    // Muestra la organizacion del sistema operativo
+    // console.log(os.type());
+
+    // Muestra la informacion de usuario
+    // console.log(os.userInfo());
+
+    // Muestra el tiempo que el sistema operativo esta en funcionamiento sin interrupciones
+    // console.log(os.uptime());
+
+    // Muestra la version del sistema operativo
+    // console.log(os.version());
+
+    // Muestra el tipo de hardware subyacente de la memoria 
+    // console.log(os.machine());
+}
+
