a22909276b8aef91eccd1658ac2018ef0b7af69b
Camilo Andres Conrado Benavides <camiloconrado1@gmail.com>
2025-11-12 10:55:31 -0500

    Refactor project structure and implement user management with Prisma
    - Updated main entry point to src/app.js
    - Added Prisma schema and migration files for User and Post models
    - Implemented user routes and controllers for CRUD operations
    - Removed unused product and user route files
    - Introduced prismaClient for database interactions
    - Added scripts for Prisma commands in package.json

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 import user from "./routes/user.js";
-import product from "./routes/product.js";
 
-app.use("/product", product);
-app.use("/user", user);
+app.use(express.json());    
 
-app.listen(PORT,()=>{
+
+app.use("/user", user );
+
+app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
     console.log(`http://127.0.0.1:${PORT}`);
     console.log(`http://[::]:${PORT}`);
---
+++ src/routes/user.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createUser, getAllUsers, getUserById, updateUser,deleteUser } from "../controllers/users.controller.js"; 
+const route = express.Router();
+
+route.get("/", getAllUsers)
+route.get("/:id", getUserById)
+route.post("/", createUser)
+route.put("/:id", updateUser)
+route.delete("/:id", deleteUser)
+
+export default route;
